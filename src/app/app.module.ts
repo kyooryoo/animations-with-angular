f7c81a1877b548658c49cfc42ec6fdb41146742d
Ling Jiang <kyooryoo@hotmail.com>
2020-12-30 12:52:38 +0800

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AnimboxComponent } from './animbox.component';
+import { MouseComponent } from './mouse.component';
+
+import { HomeComponent } from './routes/home.component';
+import { BasicComponent } from './routes/basic.component';
+import { ContainedComponent } from './routes/contained.component';
+import { AdvancedComponent } from './routes/advanced.component';
+import { NotFoundComponent } from './routes/notfound.component';
+
+const animationRoutes: Routes = [
+  { path: 'animation-home', component: HomeComponent },
+  { path: 'animation-basics', component: BasicComponent },
+  { path: 'animation-contained', component: ContainedComponent },
+  { path: 'animation-advanced', component: AdvancedComponent },
+  { path: '', redirectTo: '/animation-home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimboxComponent,
+    MouseComponent,
+    HomeComponent,
+    BasicComponent,
+    ContainedComponent,
+    AdvancedComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(animationRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
